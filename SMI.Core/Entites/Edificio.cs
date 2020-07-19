0ca4d448ddf7326c3c68fe3d89431e2d7951b5d1
Carlos Vazquez <carlos.vazquez.unach@gmail.com>
2020-07-18 21:04:43 -0500

    Comit Inicial

---
+++ SMI.Core/Entites/Edificio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SMI.Core.Entites
+{
+    public class Edificio
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int NumeroNiveles { get; set; }
+        public double Largo { get; set; }
+        public double Ancho { get; set; }
+        public DateTime FechaEdificacion { get; set; }
+    }
+}
---
+++ SMI.Core/Interfaces/IEdificiosRepository.cs
@@ -0,0 +1,15 @@
+﻿using SMI.Core.Entites;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SMI.Core.Interfaces
+{
+    public interface IEdificiosRepository
+    {
+        /// <summary>
+        /// Obtiene en listado de edificios disponibles
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<Edificio>> GetEdificios();
+    }
+}
