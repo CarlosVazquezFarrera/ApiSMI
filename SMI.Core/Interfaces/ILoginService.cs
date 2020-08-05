65ba9a2fc6f24013a9ebefbb3ed600c4fc09aebf
Carlos Vazquez <carlos.vazquez.unach@gmail.com>
2020-08-04 21:30:20 -0500

    LoginService

---
+++ SMI.Core/Interfaces/ILoginService.cs
@@ -0,0 +1,9 @@
+﻿namespace SMI.Core.Interfaces
+{
+    using SMI.Core.Entites;
+    using System.Threading.Tasks;
+    public interface ILoginService
+    {
+        Task<Response> Login(Empleado empleado);
+    }
+}
---
+++ SMI.Core/Services/LoginService.cs
@@ -0,0 +1,26 @@
+﻿namespace SMI.Core.Services
+{
+    using SMI.Core.Entites;
+    using SMI.Core.Interfaces;
+    using System;
+    using System.Threading.Tasks;
+
+    public class LoginService : ILoginService
+    {
+        private readonly IEmpleadoRepository _empleadoRepository;
+
+        public LoginService(IEmpleadoRepository empleadoRepository)
+        {
+            _empleadoRepository = empleadoRepository;
+        }
+
+        public async Task<Response> Login(Empleado empleado)
+        {
+            if(string.IsNullOrEmpty(empleado.Email) || string.IsNullOrEmpty(empleado.Password))
+            {
+                throw new Exception("Debe llenar los datos completos");
+            }
+            return await _empleadoRepository.Login(empleado);
+        }
+    }
+}
